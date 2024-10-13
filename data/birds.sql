967365632f7de732d2056ccd0fc588c3f1992436
AnjaliVanamala <87044561+AnjaliVanamala@users.noreply.github.com>
2024-10-12 20:59:36 -0400

    Update database for specific user

--- data/birds.sql
+++ data/birds.sql
@@ -1,2 +1,2 @@
-CREATE TABLE birds (name TEXT PRIMARY KEY, 
-number INT, date DATE, confidence TEXT)
+CREATE TABLE birds (name TEXT PRIMARY KEY, number INT, date DATE, confidence TEXT, username TEXT);
+CREATE TABLE users (username TEXT PRIMARY KEY, password TEXT);
